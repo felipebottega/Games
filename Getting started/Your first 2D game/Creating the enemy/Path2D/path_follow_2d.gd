35e3903e61515ec4d32d7c008a22b9215d3ca59f
felipebottega <felipebottega@gmail.com>
2025-06-14 23:38:07 -0300

    feat: último commit do path2d

--- Getting started/Your first 2D game/Creating the enemy/Path2D/path_follow_2d.gd
+++ Getting started/Your first 2D game/Creating the enemy/Path2D/path_follow_2d.gd
@@ -8,15 +8,13 @@ var nome = ""
 func _ready() -> void:
 	progress_ratio = 0
 	nome = get_parent().get_parent().name
-	print(get_parent().name, ' ', nome)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta: float) -> void:
+func _process(delta: float) -> void:
 	if nome == "P1":
 		progress_ratio += 0.001
 	elif nome == "P2":
-		progress_ratio = sin(
-			t)
+		progress_ratio = sin(t)
 		
 	t += 0.1 * delta
 	if t > PI/2:
