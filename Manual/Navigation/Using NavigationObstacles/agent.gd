fa3fc303ecf695ef2673775f1331b35332e7b40a
felipebottega <felipebottega@gmail.com>
2026-01-17 14:01:28 -0300

    feat: finalizando o Using NavigationObstacles

--- Manual/Navigation/Using NavigationObstacles/agent.gd
+++ Manual/Navigation/Using NavigationObstacles/agent.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 
 var speed: float = 100.0
+var dragging = false
+var drag_offset = Vector2.ZERO
 
 
 func _ready():
@@ -17,7 +19,19 @@ func _physics_process(_delta):
 		
 	var desired_velocity = current_agent_position.direction_to(next_path_position) * speed
 	$NavigationAgent2D.velocity = desired_velocity    # ativa o sinal
+	
+func _process(_delta):
+	if dragging:
+		global_position = get_global_mouse_position() + drag_offset
 
 func _on_navigation_agent_2d_velocity_computed(safe_velocity: Vector2) -> void:
 	velocity = safe_velocity
 	move_and_slide()
+	
+func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			dragging = true
+			drag_offset = global_position - event.position
+		else:
+			dragging = false
