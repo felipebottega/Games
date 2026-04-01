0085f7a660f28b640878133db61dbb9880634e0d
felipebottega <felipebottega@gmail.com>
2026-04-01 19:55:47 -0300

    pequenas melhorias

--- Manual/Scripting/GDScript/GDScript reference/my_class.gd
+++ Manual/Scripting/GDScript/GDScript reference/my_class.gd
@@ -1,12 +1,26 @@
+## Esta classe é incrível!!! 
+## Você precisa usar esta classe para entender o seu poder, vai mudar a sua vida!
+## @tutorial:             https://example.com/tutorial_1
 class_name amazing
 extends Node
 
 
+## Esta constante é incrível.[br]
+## O valor 1000 representa muitas coisas.
 const VALUE: int = 1000
 
 
-func compute_sum() -> int:
-	var s = 0
+## Do something for this plugin. Before using the method
+## you first have to [method initialize] [MyPlugin].[br]
+## [color=yellow]Warning:[/color] Always [method clean] after use.[br]
+## [codeblock]
+## func _ready():
+##     the_plugin.initialize()
+##     the_plugin.do_something()
+##     the_plugin.clean()
+## [/codeblock]
+func compute_sum(n: int) -> int:
+	var s = n
 	
 	for i in VALUE+1:
 		s += i
