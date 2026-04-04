368dd5d2d9addd3e5e4ab5cfb54e89c596906584
felipebottega <felipebottega@gmail.com>
2026-04-03 21:21:46 -0300

    feat: finalizando o tutorial de profile

--- Manual/Scripting/Core features/Debug/The Profiler/node.gd
+++ Manual/Scripting/Core features/Debug/The Profiler/node.gd
@@ -4,6 +4,19 @@ var s: int = 0
 var t: float = 0.0
 
 
+func _ready() -> void:
+	var result: float = 0.0
+	var start = Time.get_ticks_msec()
+	
+	for i in range(1, 999999):
+		result += 1.0/i 
+		
+	var end = Time.get_ticks_msec()
+	var loop_time = (end-start)/100.0
+
+	print("Loop time: {0} seconds".format([loop_time]))
+	print("Result = {0}".format([result]))
+
 func _process(delta: float) -> void:
 	s += 1
 	#breakpoint
@@ -14,5 +27,3 @@ func _process(delta: float) -> void:
 func foo(x: float):
 	t += x**2
 	#print([s, t])
-	
-	
