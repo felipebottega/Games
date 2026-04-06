ea534d4b2235a50bccb66221b39b68ce2d4194a6
felipebottega <felipebottega@gmail.com>
2026-04-06 20:49:22 -0300

    feat: primeiro commit do Creating script templates

---
+++ Manual/Scripting/Core features/Creating script templates/README.md
@@ -0,0 +1,1 @@
+# Creating script templates
---
+++ Manual/Scripting/Core features/Creating script templates/character_body_2d.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+
+const SPEED = 500.0
+const JUMP_VELOCITY = -1000.0
+
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var direction := Input.get_axis("ui_left", "ui_right")
+	velocity.x = direction * SPEED
+
+	move_and_slide()
