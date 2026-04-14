b696728b0dc2e9972ac94754f9a158001cbb750b
felipebottega <felipebottega@gmail.com>
2026-04-13 23:38:16 -0300

    feat: jogo do Your first 2D shader

--- Manual/Shaders/Your first shader/Your first 2D shader/node_2d.gd
+++ Manual/Shaders/Your first shader/Your first 2D shader/node_2d.gd
@@ -2,19 +2,9 @@
 extends Node2D
 
 
-var t := 0.0
-var blue_value = 1.0
+var blue_value: float = 1.0
 
-@onready var sprite1 := $Sprite2D
-@onready var sprite2 := $Sprite2D2
 
-func _ready() -> void:
-	pass
-	
 func _process(delta: float) -> void:
-	t += delta
-	#sprite1.material.set_shader_parameter("blue", abs(sin(t)))
-	sprite1.material.set_shader_parameter("aa", abs(sin(t)))
-	sprite2.material.set_shader_parameter("aa", abs(cos(t)))
-	sprite1.material.set_shader_parameter("bb", sin(t))
-	sprite2.material.set_shader_parameter("bb", cos(t))
+	blue_value += delta
+	$Sprite2D.material.set_shader_parameter("blue", abs(cos(blue_value)))
